5011dd3c6d7546a3b0a262bee9718ad0192f1fbc
Timothy Duke <timothy.daniel.duke@gmail.com>
2019-04-02 18:47:47 -0400

    SPI and BT updates - slight code clean

--- MSP_ICM/main.c
+++ MSP_ICM/main.c
@@ -9,13 +9,6 @@
  *      Establish I2C Slaves
  *      Establish Comm with Zynq
  *
- *  Interrupt - Common
- *      Poll I2C Slaves
- *      Write to Zynq
- *
- *  Interrupt - FSYNC
- *      5 Hz
- *      Pull GPIO Pin Up, wait shortly, pull GPIO Down
  */
 
 #include "msp430f5529.h"
@@ -30,18 +23,12 @@
 #define NULL 0
 #endif
 
+#define SPICLK      500000
+#define MUX_ADDRESS 0x77
+
+uint8_t transmitData = 0x00, receiveData = 0x00;
+
 typedef unsigned char uint_8t;
-
-// function prototypes
-void ClockInit(void);
-void Pin_Init(void);
-void SBIT(void);
-void IMU_Startup_Poll(void);
-void Zynq_Startup_Poll(void);
-
-#define ACCEL_ON        (0x01)
-#define GYRO_ON         (0x02)
-#define COMPASS_ON      (0x04)
 
 typedef struct IMU_Data
 {
@@ -50,8 +37,25 @@ typedef struct IMU_Data
     short mag[3];
 } IMU_Data;
 
-#define ZYNQ_ADDRESS 0x70
+// function prototypes
+void ClockInit(void);
+void Pin_Init(void);
+void SPI_Init(void);
+void IMU_Startup_Poll(void);
+void IMU_Loop(void);
+void sendByteSPI(uint8_t data);
+void shortToBytes(short in_s, unsigned char bytes[2]);
+void sendShortSPI(short input);
+void pollIMU(IMU_Data *data, int index);
+
+#define ACCEL_ON        (0x01)
+#define GYRO_ON         (0x02)
+#define COMPASS_ON      (0x04)
+
 #define SLAVE_NUMBER 10
+
+#define BOARD_INDEX     3
+#define J2_INDEX        2
 
 void main(void)
 {
@@ -63,21 +67,16 @@ void main(void)
     // Initialize I2C as receiver
     msp430_clock_init(20000000L, 2);
     msp430_i2c_enable();
-
-    // perform SBIT
-    SBIT();
+    SPI_Init();
 
     // Verify IMU Comm
     IMU_Startup_Poll();
 
     // failure moding
-    if( 1 != 1 )
+    if ( 1 != 1 )
     {
         // nothing for now
     }
-
-    // Initialized Zynq Comm - This is a spin wait
-    Zynq_Startup_Poll();
 }
 
 /*
@@ -88,16 +87,46 @@ void main(void)
 void Pin_Init(void)
 {
     P3SEL |= (BIT1 | BIT0);
+    GPIO_setAsOutputPin(
+                GPIO_PORT_P1,
+                GPIO_PIN2
+        );
+
+    GPIO_setOutputHighOnPin(
+        GPIO_PORT_P1,
+        GPIO_PIN2
+        );
+
+    GPIO_setAsPeripheralModuleFunctionInputPin(
+        GPIO_PORT_P4,
+        GPIO_PIN0 + GPIO_PIN1 + GPIO_PIN2 + GPIO_PIN3
+        );
 }
 
-/*
- * Startup Built-In-Test
- *
- * This test verifies functionality of the MSP430 device upon startup
- */
-void SBIT(void)
+void SPI_Init(void)
 {
-    // Tests Go Here
+    int returnValue;
+
+    USCI_B_SPI_initMasterParam param = {0};
+    param.selectClockSource = USCI_B_SPI_CLOCKSOURCE_SMCLK;
+    param.clockSourceFrequency = UCS_getSMCLK();
+    param.desiredSpiClock = SPICLK;
+    param.msbFirst = USCI_B_SPI_MSB_FIRST;
+    param.clockPhase = USCI_B_SPI_PHASE_DATA_CHANGED_ONFIRST_CAPTURED_ON_NEXT;
+    param.clockPolarity = USCI_B_SPI_CLOCKPOLARITY_INACTIVITY_HIGH;
+    returnValue =  USCI_B_SPI_initMaster(USCI_B1_BASE, &param);
+
+    if (STATUS_FAIL == returnValue){
+        return;
+    }
+
+    USCI_B_SPI_enable(USCI_B1_BASE);
+
+    //Now with SPI signals initialized, reset slave
+    GPIO_setOutputLowOnPin(
+        GPIO_PORT_P1,
+        GPIO_PIN2
+        );
 }
 
 volatile int count = 0;
@@ -110,14 +139,12 @@ volatile int goodCount = 0;
  */
 void IMU_Startup_Poll(void)
 {
-    unsigned char address, reg, accel_fsr, more;
-    unsigned short gyro_rate, gyro_fsr, compass_fsr;
-    volatile int Status, i, j;
+    unsigned char address;
+    unsigned short gyro_rate;
+    volatile int Status, i, j, index;
     unsigned long timestamp;
 
-    unsigned char timeArray[8];
     unsigned char outArray[6];
-    unsigned char inArray[5];
 
     IMU_Data output;
 
@@ -136,26 +163,9 @@ void IMU_Startup_Poll(void)
 
     while (1)
     {
-        address = 0x08;
+        pollIMU(&output, BOARD_INDEX);
 
-        j = msp430_i2c_write(0x77, 0, 1, &address);
-        // multiplexer write
-
-        // imu write
-        mpu_get_accel_reg(output.accel, NULL);
-        mpu_get_gyro_reg(output.gyro, NULL);
-
-        address = 0x04;
-
-        msp430_i2c_write(0x77, 0, 1, &address);
-
-        mpu_get_accel_reg(output.accel, NULL);
-        mpu_get_gyro_reg(output.gyro, NULL);
-        // multiplexer write
-
-        //mpu_get_accel_reg(output.accel, NULL);
-        //mpu_get_gyro_reg(output.gyro, NULL);
-        //mpu_get_compass_reg(output.mag, &timestamp);
+        pollIMU(&output, J2_INDEX);
 
         if (output.accel[0] == 0 || output.accel[1] == 0 || output.accel[2] == 0)
             count++;
@@ -173,11 +183,28 @@ void IMU_Startup_Poll(void)
             outArray[(2 * i) + 13]  = (output.mag[i] & 0xff);
         }*/
 
-        // SPI Write Goes Here
-        //msp430_i2c_write(ZYNQ_ADDRESS, 0, 18, outArray);
+        // Transmit over SPI
+        GPIO_setOutputLowOnPin
+        (
+            GPIO_PORT_P1,
+            GPIO_PIN2
+        );
 
-        // check a status word
-        //msp430_i2c_read(ZYNQ_ADDRESS, 0, 5, inArray);
+        gyro_rate = 1250;
+
+        address = 'A';
+        sendByteSPI(address);
+
+        sendShortSPI(gyro_rate);
+        sendShortSPI(gyro_rate);
+        sendShortSPI(gyro_rate);
+        sendShortSPI(gyro_rate);
+
+        GPIO_setOutputHighOnPin
+        (
+            GPIO_PORT_P1,
+            GPIO_PIN2
+        );
 
         if(0 >= SLAVE_NUMBER)
         {
@@ -188,11 +215,72 @@ void IMU_Startup_Poll(void)
 }
 
 /*
- * Establish initial comm with Zynq
+ * Polls a single IMU
  *
- * Spin Wait Until Successful Comm Established
+ * polls a single IMU for data
  */
-void Zynq_Startup_Poll(void)
+void pollIMU(IMU_Data *data, int index)
 {
-    //
+    unsigned char reg;
+
+    reg = (1 << index);
+
+    msp430_i2c_write(MUX_ADDRESS, 0, 1, &reg);
+
+    mpu_get_accel_reg(data->accel, NULL);
+    mpu_get_gyro_reg(data->gyro, NULL);
+}
+
+void sendShortSPI(short input)
+{
+    unsigned char splitShort[2];
+    unsigned char output;
+
+    shortToBytes(input, splitShort);
+
+    output = splitShort[1];
+    sendByteSPI(output);
+    output = splitShort[0];
+    sendByteSPI(output);
+}
+
+void shortToBytes(short in_s, unsigned char bytes[2])
+{
+    union {
+        short s;
+        unsigned char c[2];
+    }U;
+
+    U.s = in_s;
+    bytes[0] = U.c[0];
+    bytes[1] = U.c[1];
+}
+
+void sendByteSPI(uint8_t data)
+{
+    // don't send without
+    while (!USCI_B_SPI_getInterruptStatus(USCI_B1_BASE, USCI_B_SPI_TRANSMIT_INTERRUPT)) ;
+
+    USCI_B_SPI_transmitData(USCI_B1_BASE, data);
+}
+
+#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
+#pragma vector=USCI_B1_VECTOR
+__interrupt
+#elif defined(__GNUC__)
+__attribute__((interrupt(USCI_B0_VECTOR)))
+#endif
+void USCI_B1_ISR (void)
+{
+    switch (__even_in_range(UCB1IV,4)){
+        //Vector 2 - RXIFG
+        case 2:
+            //USCI_A0 TX buffer ready?
+
+            break;
+        case 4:
+            // USCI_B1 TXIFG???
+
+        default: break;
+    }
 }
